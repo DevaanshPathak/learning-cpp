da3ce4f4ce228723312e1f9beae3196b334770f3
DevaanshPathak <devaanshpathak08@gmail.com>
2026-08-25 10:41:24 +0530

    Add ques1.cpp program to demonstrate user input and output in C++

---
+++ 14day_challenge/day1/cin/ques1.cpp
@@ -0,0 +1,33 @@
+/*
+Write a program that:
+
+Creates an integer variable called age
+Takes the age as input
+Prints:
+Your age is 18
+
+For example:
+
+Input
+
+18
+
+Output
+
+Your age is 18
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int age;
+
+    cin >> age;
+
+    cout << "Your age is " << age << '\n';
+
+    return 0;
+}
